474eb8967839991ea1b1a207beb35951e5e19bf0
Niko <128721485+NikoShoo@users.noreply.github.com>
2024-11-25 00:37:36 +0400

    Подготавливает проект к защите  (#8)
    * меняет p на dl в доп товарах
    
    * Меняет верстку и добавляет несколько стилей
    
    * Создает кнопку для бургер меню
    
    * Добавляет стили для бургера
    
    * Добавляет required
    
    * Добавляет крточка секции works теги
    
    * Оставляет один тег a для двух logo в Header
    
    * Изменяет тег DD на Ul в Section experience
    
    * Меняет секцию Invitation добавляя adress
    
    * Добавляет action subscription__control
    
    * Меняет регистр букв в placeholder
    
    * Добавляет последние изменения
    
    * Делает правки  в index.html
    
    * Вносит изменения в стили текста
    
    * Меняет верстку
    
    * Добавляет декор
    
    * Добавляет svg файлы
    
    * Изменяет название проекта
    
    * Создает слайдер
    
    * Переустанавливает сборку
    
    * Вносит правки в блок как это работает
    
    * Изменяет карту
    
    * Вносит правки
    
    * вносит правки
    
    * Делает правки
    
    * финал
    
    * Исправляет замечания lint
    
    * Подгоняет размеры под pixel perfect

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
 /* в этот файл добавляет скрипты*/
+/* Бургер-меню */
+const navToggle = document.querySelector('.main-header__burger');
+const navList = document.querySelector('.navigation__list');
+
+navList.classList.remove('navigation__list--no-js');
+
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('main-header__burger--open');
+  navList.classList.toggle('navigation__list--is-opened');
+});
