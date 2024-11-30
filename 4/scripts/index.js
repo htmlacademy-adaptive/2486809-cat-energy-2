72bdd229ea5563af4f069d85d659f6a497db007c
NikoShoo <NikoShoo@users.noreply.github.com>
2024-11-30 12:14:03 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2486809-cat-energy-2@d3f9aab92ed9366088f059e5cb7728c910d48f33 🚀

--- 4/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".main-header__burger"),t=document.querySelector(".navigation__list");t.classList.remove("navigation__list--no-js");e.addEventListener("click",()=>{e.classList.toggle("main-header__burger--open"),t.classList.toggle("navigation__list--is-opened")});
